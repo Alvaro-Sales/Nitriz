0effb7be8fefb69a71377518fd37848242dc8335
Álvaro Sales Balbino <97653093+Alvaro-Sales@users.noreply.github.com>
2022-07-31 16:04:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -40,3 +40,11 @@ const mobileNavbar = new MobileNavbar(
 );
 
 mobileNavbar.init();
+
+
+let progress = document.getElementById('progressBar')
+let totalHeight = document.body.scrollHeight - window.innerHeight
+window.onscroll = function() {
+  let progressHeight = (window.pageYOffset / totalHeight) * 100
+  progress.style.height = progressHeight + '%'
+}
